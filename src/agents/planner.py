9929c0d1bbda4353709f743658f698c46dd097e8
Alexander Murphy <lordpizza11@gmail.com>
2025-03-01 15:42:19 +0000

    Adds planner

---
+++ src/agents/planner.py
@@ -0,0 +1,11 @@
+from base_agent import BaseAgent
+
+class Planner(BaseAgent):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.base_prompt = "You are the planner agent. You are given a question that you need to create a plan for, that is then executed by a series of agents. You may be given additional context of previous agent calls. If that is the case, create another plan to improve upon the final result."
+    
+    def execute(self, input, **kwargs):
+        prompt = self.base_prompt + "\nThis here is the context you have been given: <CONTEXT>\n" + input + "<\CONTEXT>\nNow create a plan which other agents can execute upon."
+        self.run(prompt)
