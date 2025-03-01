d1f53571d7065c6f5a4376b5405a9c0b540da865
Alexander Murphy <lordpizza11@gmail.com>
2025-03-01 19:33:36 +0000

    Update presenter prompt

--- src/agents/presenter.py
+++ src/agents/presenter.py
@@ -4,9 +4,9 @@ class Presenter(BaseAgent):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.base_prompt = "You are the presenter agent. You need to generate stock price predictions based on the signals provided by the interpreter agent."
+        self.base_prompt = "You are the presenter agent. You need to generate a final report based on data given to you by the developer agent.."
 
     def execute(self, signals, **kwargs):
-        prompt = self.base_prompt + "\nThese are the signals you have been given: <SIGNALS>\n" + signals + "<\SIGNALS>\nNow generate a set of stock price predictions based on these signals."
+        prompt = self.base_prompt + "\nThese are the signals you have been given: <CONTEXT>\n" + signals + "<\CONTEXT>\nNow generate a report from these signals."
         self.run(prompt)
         
